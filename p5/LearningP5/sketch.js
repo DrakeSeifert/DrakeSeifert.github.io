76fb15da8e2ead3bc24f8a4bc9c94a018538d7fa
DrakeSeifert <drakeseifert1@gmail.com>
2016-12-14 17:43:05 -0800

    cat

--- p5/LearningP5/sketch.js
+++ p5/LearningP5/sketch.js
@@ -2,12 +2,14 @@ var x = 0;
 
 function setup() {
 	createCanvas(600,400);
-	background(150);
-	//var myCanvas = createCanvas(600,400);
-	//myCanvas.parent('myContainer');
+	loadImage('cat.jpg', drawCat);
+}
+
+function drawCat(img) {
+	image(img, 0, 0);
 }
 
 function draw() {
-	ellipse(x, height/2, 20, 20);
-	x += 1;
+	//ellipse(x, height/2, 20, 20);
+	//x += 1;
 }
