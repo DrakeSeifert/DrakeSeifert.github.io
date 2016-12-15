06b2c487d2c7b1ea625dce1f130092f8f2133834
DrakeSeifert <drakeseifert1@gmail.com>
2016-12-14 18:15:52 -0800

    back to arrow keys

--- p5/Snake/sketch.js
+++ p5/Snake/sketch.js
@@ -13,22 +13,13 @@ function draw() {
 }
 
 function keyPressed() {
-	/*if (keyCode === UP_ARROW) {
+	if (keyCode === UP_ARROW) {
 		s.dir(0, -1);
 	} else if (keyCode === DOWN_ARROW) {
 		s.dir(0, 1);
 	} else if (keyCode === RIGHT_ARROW) {
 		s.dir(1, 0);
 	} else if (keyCode === LEFT_ARROW) {
-		s.dir(-1, 0);
-	}*/
-	if (keyCode == 'w') {
-		s.dir(0, -1);
-	} else if (keyCode == 's') {
-		s.dir(0, 1);
-	} else if (keyCode == 'd') {
-		s.dir(1, 0);
-	} else if (keyCode == 'a') {
 		s.dir(-1, 0);
 	}
 }
