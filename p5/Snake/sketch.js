153ffbcfd5198dff871e8c5e9c2494dd73b4714f
DrakeSeifert <drakeseifert1@gmail.com>
2016-12-14 17:59:14 -0800

    snake!

--- p5/Snake/sketch.js
+++ p5/Snake/sketch.js
@@ -1,12 +1,29 @@
+var s;
+
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(600, 600);
+  s = new Snake();
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+	background(51);
+	s.update();
+	s.show();
+}
+
+function Snake() {
+	this.x = 0;
+	this.y = 0;
+	this.xspeed = 1;
+	this.yspeed = 0;
+
+	this.update = function() {
+		this.x += this.xspeed;
+		this.y += this.yspeed;
+	}
+
+	this.show = function() {
+		fill(255);
+		rect(this.x, this.y, 10, 10);
+	}
 }
